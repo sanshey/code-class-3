5b8d420900a577742691c7d449d961764dad1f01
Sanshey Biswas <SB@Sansheys-MacBook-Pro.local>
2016-10-12 17:56:33 +0530

    class 3 ends

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
-console.log("My javascript is working.")
+//This is my javascript. I want to see if it's working
+
+// console.log("My javascript is working.")
+
+
+//In Javascript, there are five data types
+
+//1) A string, which is a sequence of characters
+
+var myString = "Hello world!";
+//console.log(myString);
+
+//2) A number
+
+var myNumber = 24;
+//console.log(myNumber);
+//console.log(myNumber+70);
+//console.log(myNumber.toString()+6);
+
+//3) An Arra, which is a fancy word for a list
+
+var myArray = ["a","b","c","d"];
+// console.log(myArray);
+// console.log(myArray[3]);
+// console.log(myArray.length);
+myArray.push("Harry");
+// console.log(myArray);
+myArray.push(myNumber);
+// console.log(myArray);
+
+//4) An object, which is a set of key value pairs
+
+var myObject={
+  name: "Sanshey",
+  age: 24,
+  classmates: myArray
+}
+
+//5) A boolean, which is true or false
+var myBool = true;
+var myOtherbool = false;
